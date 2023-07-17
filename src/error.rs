4eb68eb4ff4a501c7844d3967e040b5d35f0cb3c
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-07-17 11:40:05 +0000

    feat: Add support to configs in YAML format (#1)
    * chore: Initial preparation
    
    * refactor: Separate function from the JSON format
    
    * feat: Add support to configs in YAML format
    
    * chore: Update README

--- src/error.rs
+++ src/error.rs
@@ -5,9 +5,15 @@ pub enum Error {
     #[error("Configuration file: {source}")]
     FileNotFound { source: std::io::Error },
 
+    #[error("Unsupported configuration file format: {format}")]
+    UnsupportedFormat { format: String },
+
     #[error("Environment value {env} is not present and no default value was provided")]
     DefaultMissing { env: String },
 
     #[error("Error parsing configuration file: {0}")]
     JsonParse(#[from] serde_json::Error),
+
+    #[error("Error parsing configuration file: {0}")]
+    YamlParse(#[from] serde_yaml::Error),
 }
