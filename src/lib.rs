838db004e131e8639e26ae55a6ffd83e0e4c7069
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2023-09-14 13:50:25 -0300

    toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,101 @@
-use std::fs::File;
+use std::collections::HashMap;
 use std::path::Path;
 
-use serde::de::DeserializeOwned;
+use serde::{de, Deserialize, Serialize};
+use serde_untagged::UntaggedEnumVisitor;
+use toml::Value;
 
-use crate::error::{Error, KonfigurationResult};
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ConfigurationManifest(HashMap<String, ConfigurationEntry>);
 
-mod env_vars;
-mod error;
-mod json;
-mod utils;
-mod yaml;
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+pub enum ConfigurationEntry {
+    Simple(Value),
+    Detailed(DetailedConfigurationEntry),
+    Table(HashMap<String, ConfigurationEntry>),
+}
 
-/// Serialize and Deserialize configuration files.
-///
-/// # Example
-///
-/// In your config.json file:
-/// ```json
-/// {
-///     "server_port": 8080,
-///     "database": {
-///         "host": "localhost",
-///         "username": "root",
-///         "password": "password"
-///     }
-/// }
-/// ```
-///
-/// In your main.rs file:
-/// ```rust
-/// use konfiguration::Konfiguration;
-///
-/// #[derive(Debug, serde::Deserialize)]
-/// pub struct DatabaseConfig {
-///     pub host: String,
-///     pub username: String,
-///     pub password: String,
-/// }
-///
-/// #[derive(Debug, serde::Deserialize)]
-/// pub struct AppConfig {
-///     pub server_port: u16,
-///     pub database: DatabaseConfig,
-/// }
-///
-/// let config = Konfiguration::from_file("test_files/json/config.json").parse::<AppConfig>().unwrap();
-///
-/// println!("{:#?}", config);
-///
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct DetailedConfigurationEntry {
+    env: Option<String>,
+    default: Value,
+}
+
+impl<'de> de::Deserialize<'de> for ConfigurationEntry {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        UntaggedEnumVisitor::new()
+            .expecting(
+                "a configuration like \"0.9.8\" or a \
+                     detailed configuration like { env = \"REPLACE_WITH_THIS_ENV\", version = \"0.9.8\" }",
+            )
+            .i8(|value| {
+                Ok(ConfigurationEntry::Simple(Value::Integer(
+                    value.into(),
+                )))
+            })
+            .i16(|value| {
+                Ok(ConfigurationEntry::Simple(Value::Integer(
+                    value.into(),
+                )))
+            })
+            .i32(|value| {
+                Ok(ConfigurationEntry::Simple(Value::Integer(
+                    value.into(),
+                )))
+            })
+            .i64(|value| {
+                Ok(ConfigurationEntry::Simple(Value::Integer(
+                    value.into(),
+                )))
+            })
+            .u8(|value| {
+                Ok(ConfigurationEntry::Simple(Value::Integer(
+                    value.into(),
+                )))
+            })
+            .u16(|value| {
+                Ok(ConfigurationEntry::Simple(Value::Integer(
+                    value.into(),
+                )))
+            })
+            .f32(|value| Ok(ConfigurationEntry::Simple(Value::Float(value.into()))))
+            .f64(|value| Ok(ConfigurationEntry::Simple(Value::Float(value.into()))))
+            .char(|value| {
+                Ok(ConfigurationEntry::Simple(Value::String(
+                    value.to_string(),
+                )))
+            })
+            .string(|value| {
+                Ok(ConfigurationEntry::Simple(Value::String(
+                    value.to_string(),
+                )))
+            })
+            .map(|value| {
+                let map: Value = value.deserialize()?;
+                let mut map = map.as_table().unwrap().clone();
+
+                if map.contains_key("env") || map.contains_key("default") {
+                    Ok(ConfigurationEntry::Detailed(DetailedConfigurationEntry {
+                        env: map
+                            .remove("env")
+                            .and_then(|v| v.as_str().map(|s| s.to_string())),
+                        default: map.remove("default").unwrap(),
+                    }))
+                } else {
+                    let str = toml::to_string(&map).unwrap();
+
+                    Ok(ConfigurationEntry::Table(toml::from_str(&str).unwrap()))
+                }
+            })
+            .deserialize(deserializer)
+    }
+}
+
 pub struct Konfiguration {
     file_path: String,
 }
@@ -59,24 +107,78 @@ impl Konfiguration {
         }
     }
 
-    pub fn parse<T: DeserializeOwned>(self) -> KonfigurationResult<T> {
+    pub fn parse(self) -> anyhow::Result<()> {
         let path = Path::new(&self.file_path);
 
-        let file_extension =
-            path.extension()
-                .and_then(|ext| ext.to_str())
-                .ok_or(Error::FileNotFound {
-                    path: self.file_path.clone(),
-                })?;
+        let string = std::fs::read_to_string(path)?;
 
-        let file = File::open(path)?;
+        let manifest: ConfigurationManifest = toml::from_str(&string)?;
 
-        match file_extension {
-            "json" => json::parse(file),
-            "yml" | "yaml" => yaml::parse(file),
-            format => Err(Error::UnsupportedFormat {
-                format: format.to_string(),
-            }),
-        }
+        let mut config = toml::map::Map::new();
+
+        expand_env_vars(&mut config, manifest);
+
+        println!("{config:#?}");
+
+        Ok(())
+    }
+}
+
+fn expand_env_vars(configs: &mut toml::map::Map<String, Value>, manifest: ConfigurationManifest) {
+    for (name, entry) in manifest.0 {
+        match entry {
+            ConfigurationEntry::Simple(v) => {
+                configs.insert(name, v);
+            }
+            ConfigurationEntry::Detailed(DetailedConfigurationEntry { env, default }) => {
+                if let Some(env) = env {
+                    let env_val = std::env::var(env).ok();
+
+                    match env_val {
+                        None => configs.insert(name, default),
+                        Some(var) => match default {
+                            Value::String(_) => configs.insert(name, Value::String(var)),
+                            Value::Integer(_) => {
+                                configs.insert(name, Value::Integer(var.parse().unwrap()))
+                            }
+                            Value::Float(_) => {
+                                configs.insert(name, Value::Float(var.parse().unwrap()))
+                            }
+                            Value::Boolean(_) => {
+                                configs.insert(name, Value::Boolean(var.parse().unwrap()))
+                            }
+                            Value::Datetime(_) => {
+                                configs.insert(name, Value::Datetime(var.parse().unwrap()))
+                            }
+                            Value::Array(_) => {
+                                continue;
+                            }
+                            Value::Table(_) => unreachable!(
+                                "this should be handled by the table stuff, how did it get here?"
+                            ),
+                        },
+                    };
+                } else {
+                    configs.insert(name, default);
+                }
+            }
+            ConfigurationEntry::Table(t) => {
+                let mut nested = toml::map::Map::new();
+
+                expand_env_vars(&mut nested, ConfigurationManifest(t));
+
+                configs.insert(name, Value::Table(nested));
+            }
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let _ = Konfiguration::from_file("test_files/configs.toml").parse();
     }
 }
